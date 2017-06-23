c7a6135260dd5db373bf0667615163da10024b2d
Sanjeev Ghimire <sanjeevghimire@Sanjeevs-MacBook-Pro.local>
2017-06-23 16:05:02 -0500

    swift app for real estate bot

---
+++ Real Estate/Credentials.swift
@@ -0,0 +1,12 @@
+
+public struct Credentials {
+    public static let ConversationUsername = "09e1f7ca-58b1-4dfb-ae02-a8404f7efdeb"
+    public static let ConversationPassword = "BoVs7Lk6cb2j"
+    public static let ConversationWorkspace = "ca8d8359-7a87-4da4-9eb2-7f38e785ccd1"
+    
+    public static let SpeechToTextUsername = "df0021f3-7393-4b95-a41a-b7699d2ab67e"
+    public static let SpeechToTextPassword = "W7YmQek66TRD"
+    
+    public static let TextToSpeechUsername = "8a27d2da-fb78-4024-b3a2-4a98ec3d40f5"
+    public static let TextToSpeechPassword = "EgM5bPqS7mds"
+}
