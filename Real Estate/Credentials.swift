7bdc73eb66a489dcb94cb42fdbf24949b7e9b1a2
Sanjeev Ghimire <gsanjeev7@gmail.com>
2017-06-23 16:25:12 -0500

    Update Credentials.swift

--- Real Estate/Credentials.swift
+++ Real Estate/Credentials.swift
@@ -1,12 +1,11 @@
-
 public struct Credentials {
-    public static let ConversationUsername = "09e1f7ca-58b1-4dfb-ae02-a8404f7efdeb"
-    public static let ConversationPassword = "BoVs7Lk6cb2j"
-    public static let ConversationWorkspace = "ca8d8359-7a87-4da4-9eb2-7f38e785ccd1"
     
-    public static let SpeechToTextUsername = "df0021f3-7393-4b95-a41a-b7699d2ab67e"
-    public static let SpeechToTextPassword = "W7YmQek66TRD"
-    
-    public static let TextToSpeechUsername = "8a27d2da-fb78-4024-b3a2-4a98ec3d40f5"
-    public static let TextToSpeechPassword = "EgM5bPqS7mds"
+    public static let ConversationUsername = “<conversation username>"
+    public static let ConversationPassword = “<conversation password>"
+    public static let ConversationWorkspace = “<conversation workspace id>"
+    public static let SpeechToTextUsername = “<speech to text username>”
+    public static let SpeechToTextPassword = “<speech to text password>"
+    public static let TextToSpeechUsername = “<text to speech username>"
+    public static let TextToSpeechPassword = “<text to speech password>"
+
 }
